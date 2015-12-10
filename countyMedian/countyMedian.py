5d094e85c0dc0a5ca2bcc751fdd80fa3a10f3350
azhong <azhong@college.harvard.edu>
2015-12-10 17:36:13 -0500

    adding website files

--- countyMedian/countyMedian.py
+++ countyMedian/countyMedian.py
@@ -9,27 +9,43 @@ def countyMedian(pkl, col) :
     #processing zip codes
     lst=[]
     for i in test["zip"]:
+        #print i
         if len(str(i))!=5:
             lst.append("0"+str(i))
         else:
             lst.append(str(i))
 
+    test["zip"] = lst
     newcountyCodes=pd.read_csv('countyMedian/zip_to_county.csv', dtype={'ZCTA5': object, "GEOID" : object})
     newcountyCodes = newcountyCodes.rename(columns={'ZCTA5': 'zip'})
     newcountyCodes['zip'] = newcountyCodes['zip'].astype(str)
+    mapcountyCodes=pd.read_csv('countyMedian/US_FIPS_Codes.csv', dtype={'FIPS State': object, "FIPS County" : object})
+    mapcountyCodes["GEOID"] = mapcountyCodes["FIPS State"] + mapcountyCodes["FIPS County"]
+ 
+    db_tmp = pd.merge(test, newcountyCodes, on='zip')
+    db = pd.merge(db_tmp, mapcountyCodes[["State", "County Name", "GEOID"]], on="GEOID")
 
-    db = pd.merge(test, newcountyCodes, on='zip')
-
+    #adding state abbreviations
+    with open('addZipCodes/states-abbrev.json') as data_file:
+        data = json.loads(data_file.read())
+    
+    state_lst = []
+    for i in db.iterrows():
+        state_lst.append(data[i[1]["State"]])
+    db["State_Abbrev"] = state_lst
+        
     def agg_metric(metric):
-        gb = db[[metric, "GEOID"]].groupby("GEOID")
+        gb = db[[metric, "GEOID", "County Name", "State_Abbrev", "State"]].groupby(["GEOID", "County Name", "State_Abbrev", "State"])
         medians = gb.agg([np.median])
-        medians.head()
 
         #processing into json format
         lst = []
         for i in medians.iterrows():
             dct = {}
-            dct["id"] = i[0]
+            dct["id"] = i[0][0]
+            dct["state_name"] = i[0][3]
+            dct["state_id"] = i[0][2]
+            dct["county"] = i[0][1]
             if np.isnan(i[1][0]):
                 dct["value"] = None
             else:
@@ -41,51 +57,12 @@ def countyMedian(pkl, col) :
                 i["id"] = i["id"][1:]
 
         import json
-        fd=open("data/"+metric+".json","w")
+        fd=open("website/data/"+metric+"_new.json","w")
         json.dump(lst, fd)
         fd.close()
 
         return medians, lst
 
-    medians, lst = agg_metric(col)
+    medians, new_lst = agg_metric(col)
 
-    med_list = []
-    for i in medians.iterrows():
-        if np.isnan(i[1][0]):
-            med_list.append(np.nan)
-        else:
-            med_list.append(i[1][0])
-
-    s=pd.Series(med_list)
-
-    #get 2011 house price data
-    lst = []
-    for i in medians.iterrows():
-        dct = {}
-        dct["id"] = i[0]
-        if np.isnan(i[1][0]):
-            dct["value"] = None
-        else:
-            dct["value"] = i[1][0]
-        lst.append(dct)
-
-    import json
-    fd=open("countyMedian/houses_2011_3.json","w")
-    json.dump(lst, fd)
-    fd.close()
-
-    countyCodes=pd.read_csv('countyMedian/US_FIPS_Codes.csv')
-    i = countyCodes[["FIPS State", "FIPS County"]]
-    FIPs = []
-    for row in i.iterrows():
-        FIPs.append(('%02d' % row[1][0]) + ('%03d' % row[1][1]))
-    countyCodes["FIP_Code"] = FIPs
-
-    county_srted = db.sort('STATE')
-    county_srted.head()
-
-    new_db = pd.merge(db, newcountyCodes, on=['zip','GEOID','STATE','COUNTY'])
-    gb = new_db.groupby("GEOID")
-    new = gb.agg([np.median])
-
-    return new[col].dropna()
+    return db, new_lst
